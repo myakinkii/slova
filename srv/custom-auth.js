3ba89460c3c4fc4ce2844bc81162b41878a45166
alex <alexey.myakinky@dev4hana.ru>
2023-07-02 14:40:30 +0200

    add simple fake users toggle

--- srv/custom-auth.js
+++ srv/custom-auth.js
@@ -1,4 +1,6 @@
 const cds = require('@sap/cds')
+const ALLOW_FAKE_USERS = !!process.env.ALLOW_FAKE_USERS // any non-empty value is true
+
 module.exports = async function custom_auth (req, res, next) {
     if (req.baseUrl=='/onboard') return next() // wtf it does not require auth...
     const auth = req.header('Authorization')
@@ -13,7 +15,7 @@ module.exports = async function custom_auth (req, res, next) {
         const db = await cds.connect('db')
         let user = await db.read('cc.slova.model.Users',{id:creds[0]})
         if (user && user.pwd != creds[1] ) return res.sendStatus(401)
-        if (!user) user = { id : creds[0] } // allow fake users to login
+        if (!user && ALLOW_FAKE_USERS) user = { id : creds[0] } // allow fake users to login
         req.user = new cds.User({ id: user.id, roles: user.id=='admin' ? ['admin-user'] : [] })
         next()
     } catch (e) {
