d14e7e45ab4f2e98c0d31b81bec1a01d297717b1
alex <alexey.myakinky@dev4hana.ru>
2023-07-18 21:30:42 +0200

    workaround for missing db commit blocking main thread
    also formatted js

--- srv/texts-service.js
+++ srv/texts-service.js
@@ -15,7 +15,7 @@ class TextsService extends BaseService {
         this.on('generateText', this.generateTextHandler)
         this.on('createText', this.createTextHandler)
         this.on('getDefinition', this.getDefinitionUrl)
-        this.before('READ', 'Texts', async (req) => { 
+        this.before('READ', 'Texts', async (req) => {
             // to ensure anonymous singleton works
             await this.getProfile(req.user.id)
         })
@@ -26,10 +26,10 @@ class TextsService extends BaseService {
     }
 
     async getDefinitionUrl(req, next) {
-        const {lang, lemma} = req.data
+        const { lang, lemma } = req.data
         const profile = await this.getProfile(req.user.id)
         const userLang = profile.id == "anynoumous" ? "auto" : profile.defaultLang_code
-        let definitionUrl = await definitionFinder.get(lang, lemma).catch( ()=>{} )
+        let definitionUrl = await definitionFinder.get(lang, lemma).catch(() => { })
         if (!definitionUrl) return ''
         const googleTranslateBaseUrl = 'https://translate.google.com/translate'
         return `${googleTranslateBaseUrl}?u=${encodeURIComponent(definitionUrl)}&sl=${lang}&tl=${userLang}&hl=${userLang}`
@@ -49,37 +49,46 @@ class TextsService extends BaseService {
         else data.sent.translation = link
     }
 
-    async createTextHandler(req,next){
+    async createTextHandler(req, next) {
         if (req.user.id == 'anonymous') throw new Error('FORBIDDEN')
         const profile = await this.getProfile(req.user.id)
         const { Import } = cds.entities("cc.slova.model")
         const ID = cds.utils.uuid()
         const textNameCreated = new Intl.DateTimeFormat('en', { dateStyle: 'medium', timeStyle: 'medium', hour12: false }).format(new Date());
         await cds.create(Import).entries({ ID: ID, name: textNameCreated, input: req.data.input, lang_code: profile.defaultLang_code, createdBy: profile.id })
-        return { ID : ID }
+        return { ID: ID }
     }
 
-    async parseTextHandler(req,next){
+    async parseTextHandler(req, next) {
         const ID = req.params[0]
         const { Import } = cds.entities("cc.slova.model")
         const data = await cds.read(Import, ID)
         if (req.user.id != data.createdBy) throw new Error('FORBIDDEN')
         if (!data.input) return
-        const input = data.input.split("\n").filter( sent => !!sent )
+        const input = data.input.split("\n").filter(sent => !!sent)
+        // here we also have slow child_process.spawn, but we will leave this guy blocking for now
         const text = await this.importHandler.parseMultiline(input, data.lang_code)
-        await cds.update(Import, ID).with({text})
+        await cds.update(Import, ID).with({ text })
         const profile = await this.getProfile(req.user.id)
         await this.importHandler.parseInput(ID, profile.pos)
     }
 
-    async generateTextHandler(req,next){
+    async generateTextHandler(req, next) {
         const ID = req.params[0]
-        const { Import } = cds.entities("cc.slova.model")
-        const data = await cds.read(Import, ID)
-        if (req.user.id != data.createdBy) throw new Error('FORBIDDEN')
-        const profile = await this.getProfile(req.user.id)
+        const { Import, Users } = cds.entities("cc.slova.model")
+        // by default cds waits for all our stuff to finish and then commit
+        // but we have a slow network request in between of two db queries
+        // and in this case missing commit will block the main thread
+        // so we will use this cds.tx() magic to commit before calling chatgpt
+        // https://cap.cloud.sap/docs/node.js/cds-tx#srv-tx-fn
+        let data, profile
+        await cds.tx(async (tx) => {
+            data = await tx.read(Import, ID)
+            if (req.user.id != data.createdBy) throw new Error('FORBIDDEN')
+            profile = await tx.read(Users, { id: req.user.id })
+        })
         const chatGptResponse = await this.importHandler.callExternalGenerator(data.lang_code, profile.gptSize, profile.gptType, profile.gptLocation, profile.gptModifier)
-        return cds.update(Import, ID).with({ input : chatGptResponse.replaceAll('\n\n','\n') })
+        return cds.update(Import, ID).with({ input: chatGptResponse.replaceAll('\n\n', '\n') })
     }
 
     async syncAndReparseConllu(req, next) {
@@ -88,7 +97,7 @@ class TextsService extends BaseService {
         const importData = await this.importHandler.getImportData(token.importId)
         if (req.user.id != importData.createdBy) throw new Error('FORBIDDEN')
         const text = this.importHandler.mergeAndCreateConllu(token, importData.sentences)
-        await cds.update(Import, token.importId).with({text})
+        await cds.update(Import, token.importId).with({ text })
         const profile = await this.getProfile(req.user.id)
         await this.importHandler.parseInput(token.importId, profile.pos)
     }
