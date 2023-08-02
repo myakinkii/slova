eacc7f0e90a5c940772b68bb9341f803ac16bb2b
alex <alexey.myakinky@dev4hana.ru>
2023-08-02 22:16:01 +0200

    naive redeploy fix

--- srv/init.js
+++ srv/init.js
@@ -1,5 +1,5 @@
 const ImportHandler = require('./lib/importHandler')
-const {parseConllu, prepareWords, performMerge} = require('./lib/conlluParser')
+const { parseConllu, prepareWords, performMerge } = require('./lib/conlluParser')
 const crypto = require('crypto')
 const fs = require('fs')
 
@@ -13,28 +13,27 @@ const SET_ADMIN_PWD = process.env.SET_ADMIN_PWD // to reset admin pwd from defau
 
 module.exports = async (db) => {
     const { Stat, Slova, Sentences, Etymology, Users, Import } = db.entities('cc.slova.model')
-    const run = []
 
     if (CONLLU_SETS.length) {
         let sentences = {}, stat = [], words = [] // references to data
-        CONLLU_SETS.forEach( set => { ImportHandler.IMPORT_POS.reduce( getParser(set, sentences, stat), words) })
+        CONLLU_SETS.forEach(set => { ImportHandler.IMPORT_POS.reduce(getParser(set, sentences, stat), words) })
         sentences = Object.values(sentences)
         LOG.debug(`got ${words.length} words and ${sentences.length} sentences`)
 
-        if (words.length>0 && sentences.length>0 ){
-            run.push(
+        if (words.length > 0 && sentences.length > 0) {
+            await cds.run[
                 INSERT.into(Slova).entries(words),
                 INSERT.into(Sentences).entries(sentences),
                 INSERT.into(Stat).entries(stat)
-            )
+            ]
         }
     } else if (CONLLU_DIRS.length) {
         const importHandler = new ImportHandler(cds)
-        await Promise.all(CONLLU_DIRS.map( async set => {
-            const [ lang, dir ] = set.split("_")
+        await Promise.all(CONLLU_DIRS.map(async set => {
+            const [lang, dir] = set.split("_")
             const files = await readFolder(lang, dir)
             LOG.debug(`got ${files.length} files in ${dir}`)
-            for( var file of files){
+            for (var file of files) {
                 LOG.debug(`importing [${lang}] ${file}`)
                 const ID = cds.utils.uuid()
                 await importText(ID, lang, dir, file)
@@ -52,37 +51,38 @@ module.exports = async (db) => {
         }))
     }
 
+    const admin = await cds.read(Users, { id: 'admin' })
+    if (admin) return // redeploy
+
     const etymology = [
         { root: 'gʰóstis', ascii: 'gostis', reference: 'https://en.wiktionary.org/wiki/Reconstruction:Proto-Indo-European/g%CA%B0%C3%B3stis' },
         { root: 'slovo', ascii: 'slovo', reference: 'https://en.wiktionary.org/wiki/Reconstruction:Proto-Slavic/slovo' }
     ]
-    run.push(
+    await cds.run(
         INSERT.into(Etymology).entries(etymology)
     )
 
     let pwd = 'secret'
-    if (typeof SET_ADMIN_PWD=='string'){ // replace default one
+    if (typeof SET_ADMIN_PWD == 'string') { // replace default one
         if (SET_ADMIN_PWD) pwd = SET_ADMIN_PWD // which one to set
         else pwd = crypto.createHash('md5').update(cds.utils.uuid()).digest("hex") // or generate
-    } 
-    LOG.info('admin pwd',pwd)
+    }
+    LOG.info('admin pwd', pwd)
     const users = [{ id: 'admin', pwd: pwd, defaultLang_code: 'en' }]
-    run.push(
+    await cds.run(
         INSERT.into(Users).entries(users)
     )
 
-    return cds.run(run)
-
-    function getParser(setName, sentencesRef, statRef){
-        const [ lang, dir ] = setName.split("_")
+    function getParser(setName, sentencesRef, statRef) {
+        const [lang, dir] = setName.split("_")
         let dataSet = null
-        for (const chunk of SET_CHUNKS){
+        for (const chunk of SET_CHUNKS) {
             try {
                 const data = require('fs').readFileSync(`./test/conllu/${lang}/${dir}/${setName}-ud-${chunk}.conllu`, 'utf8')
                 LOG.debug('parsing conllu set', setName, chunk)
                 dataSet = parseConllu(lang, data)
                 Object.assign(sentencesRef, dataSet.sentences)
-                statRef.push.apply(statRef,Object.values(dataSet.stat))
+                statRef.push.apply(statRef, Object.values(dataSet.stat))
                 break;
             } catch (err) {
                 LOG.debug(`${setName} ${chunk} error`, err.message)
@@ -92,15 +92,15 @@ module.exports = async (db) => {
         return (prev, cur) => prepareWords(lang, dataSet.words, cur, prev)
     }
 
-    async function readFolder(lang, dir){
-        return new Promise((resolve,reject)=>{
-            fs.readdir(`./test/conllu/${lang}/${dir}`,(err, files) => err ? reject(err) : resolve(files) )
+    async function readFolder(lang, dir) {
+        return new Promise((resolve, reject) => {
+            fs.readdir(`./test/conllu/${lang}/${dir}`, (err, files) => err ? reject(err) : resolve(files))
         })
     }
 
-    async function importText (ID, lang, dir, fileName) {
+    async function importText(ID, lang, dir, fileName) {
         const data = fs.readFileSync(`./test/conllu/${lang}/${dir}/${fileName}`, 'utf8')
-        const name = dir+" - "+fileName.slice(0,-7)
+        const name = dir + " - " + fileName.slice(0, -7)
         return cds.create(Import).entries({ ID: ID, name: name, text: data, lang_code: lang, createdBy: 'admin' })
     }
 
