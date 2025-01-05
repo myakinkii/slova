dea1c5fb009206cab24eaf670fffcfcf8431804f
alex <alexey.myakinky@dev4hana.ru>
2025-01-05 18:43:07 +0100

    remove duplicate swagger plugin init

--- srv/server.js
+++ srv/server.js
@@ -3,7 +3,7 @@ module.exports = cds.server
 
 if (process.env.NODE_ENV !== 'production') {
     cds.on('bootstrap', app => {
-        app.use(require('cds-swagger-ui-express')())
+        // app.use(require('cds-swagger-ui-express')())
         app.use(require('cors')())
     })
 }
